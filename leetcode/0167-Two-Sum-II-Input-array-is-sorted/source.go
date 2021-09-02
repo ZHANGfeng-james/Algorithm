5ac6d977ca308ca4d0797e800ef5c44ef1e2a9d8
ZhangFeng <zfengwust3054@163.com>
2021-09-02 15:52:10 +0800

    新增：时间和空间复杂度分析

--- leetcode/0167-Two-Sum-II-Input-array-is-sorted/source.go
+++ leetcode/0167-Two-Sum-II-Input-array-is-sorted/source.go
@@ -1,5 +1,15 @@
 package leetcode
 
+import "fmt"
+
+const (
+	topic = "Leetcode Problem 0167 两数之和 II - 输入有序数组\n"
+)
+
+func init() {
+	fmt.Println(topic)
+}
+
 func twoSum(numbers []int, target int) []int {
 	m := make(map[int]int)
 	for i := 0; i < len(numbers); i++ {
@@ -8,6 +18,24 @@ func twoSum(numbers []int, target int) []int {
 			return []int{idx + 1, i + 1}
 		}
 		m[numbers[i]] = i
+	}
+	return nil
+}
+
+func otherSolution(numbers []int, target int) []int {
+	start, end := 0, len(numbers)-1
+	for start < end {
+		sub := target - numbers[start]
+		if sub == numbers[end] {
+			return []int{start + 1, end + 1}
+		}
+
+		// [1 2 3 4] 6
+		if sub > numbers[end] {
+			start++
+		} else {
+			end--
+		}
 	}
 	return nil
 }
--- leetcode/0167-Two-Sum-II-Input-array-is-sorted/source_test.go
+++ leetcode/0167-Two-Sum-II-Input-array-is-sorted/source_test.go
@@ -22,16 +22,16 @@ type ans struct {
 func TestProblem(t *testing.T) {
 	qs := []question{
 		{
-			para{[]int{3, 2, 4}, 6},
-			ans{[]int{2, 3}},
+			para{[]int{2, 3, 4}, 6},
+			ans{[]int{1, 3}},
 		},
 		{
-			para{[]int{3, 2, 4}, 5},
+			para{[]int{2, 3, 4}, 5},
 			ans{[]int{1, 2}},
 		},
 		{
-			para{[]int{0, 8, 7, 3, 3, 4, 2}, 11},
-			ans{[]int{2, 4}},
+			para{[]int{0, 2, 3, 5, 7, 8}, 3},
+			ans{[]int{1, 3}},
 		},
 		{
 			para{[]int{0, 1}, 1},
@@ -44,11 +44,9 @@ func TestProblem(t *testing.T) {
 		// 如需多个测试，可以复制上方元素。
 	}
 
-	fmt.Printf("--------Leetcode Problem 0167-------\n")
-
 	for _, q := range qs {
 		_, p := q.ans, q.para
-		fmt.Printf("[input]:%v;[got]:%v;[want]:%v\n\n", p, twoSum(p.nums, p.target), q.ans)
+		fmt.Printf("[input]:%v;[got]:%v;[want]:%v\n\n", p, otherSolution(p.nums, p.target), q.ans)
 	}
 	fmt.Printf("\n")
 }
