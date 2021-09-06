4657908c5a63b7db528b51a89ce43565c643fb73
ZhangFeng <zfengwust3054@163.com>
2021-09-06 14:24:57 +0800

    commit amend

--- leetcode/0021-Merge-Two-Sorted-Lists/source.go
+++ leetcode/0021-Merge-Two-Sorted-Lists/source.go
@@ -23,11 +23,12 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 
 	if l1.Val < l2.Val {
-		// 本次取得 l1 作为当前处理的结果（“归”），取 l1，同时“递”
+		// “递”，本次取得 l1 作为当前处理的结果（“归”），取 l1
 		l1.Next = mergeTwoLists(l1.Next, l2)
 		return l1
 	} else {
 		// l1.Val >= l2.Val，取 l2
+		// “递”，本次取得 l2 作为当前处理的结果（“归”），取 l2
 		l2.Next = mergeTwoLists(l1, l2.Next)
 		return l2
 	}
