f094d902293edf63f1b9226aff1e9e16873b94e4
ZhangFeng <zfengwust3054@163.com>
2021-09-07 19:01:15 +0800

    add: 0101-Symmetric-Tree

--- leetcode/0101-Symmetric-Tree/source.go
+++ leetcode/0101-Symmetric-Tree/source.go
@@ -2,28 +2,33 @@ package leetcode
 
 import (
 	"fmt"
+	"math"
 )
 
 const (
-	topic = "Leetcode Problem 0094. 二叉树的中序遍历\n"
+	topic = "Leetcode Problem 0101. 对称二叉树\n"
 )
 
 func init() {
 	fmt.Println(topic)
 }
 
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	tmp := inorderTraversal(root)
+
+	length := len(tmp)
+	for index, ele := range tmp {
+		if ele != tmp[length-index-1] {
+			return false
+		}
+	}
+	return true
 }
 
 func inorderTraversal(root *TreeNode) []int {
@@ -31,15 +36,25 @@ func inorderTraversal(root *TreeNode) []int {
 		return nil
 	}
 
-	// [1,null,2]
+	// [1,2,2,2,null,2]
+
 	result := make([]int, 0)
+
+	if root.Left == nil && root.Right == nil {
+		result = append(result, root.Val)
+		return result
+	}
 
 	if root.Left != nil {
 		result = append(result, inorderTraversal(root.Left)...)
+	} else {
+		result = append(result, math.MaxInt32)
 	}
 	result = append(result, root.Val)
 	if root.Right != nil {
 		result = append(result, inorderTraversal(root.Right)...)
+	} else {
+		result = append(result, math.MaxInt32)
 	}
 	return result
 }
