2b8e97ec94e82b991ef9b5010cca39a794f5b058
ZhangFeng <zfengwust3054@163.com>
2021-09-10 09:34:25 +0800

    新增：0155-Min-Stack 和 0160-Intersection-of-Two-Linked-Lists

---
+++ leetcode/0160-Intersection-of-Two-Linked-Lists/source_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+type question struct {
+	para
+	ans
+}
+
+type para struct {
+	nums   []int
+	target int
+}
+
+type ans struct {
+	one []int
+}
+
+func TestProblem(t *testing.T) {
+
+	fmt.Printf("\n")
+}
