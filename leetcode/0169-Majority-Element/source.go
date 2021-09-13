82cb251134001d2856d9e7d34d3eb73a4d4f414e
ZhangFeng <zfengwust3054@163.com>
2021-09-13 18:39:56 +0800

    新增：0169-Majority-Element

---
+++ leetcode/0169-Majority-Element/source.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"fmt"
+)
+
+const (
+	topic = "Leetcode Problem 0169. 多数元素\n"
+)
+
+func init() {
+	fmt.Println(topic)
+}
+
+func majorityElement(nums []int) int {
+
+}
