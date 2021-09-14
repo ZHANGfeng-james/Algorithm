18ca8a661d3e7a31f39d2861d8d9e8874095e0d6
ZhangFeng <zfengwust3054@163.com>
2021-09-14 10:28:36 +0800

    新增：0206-Reverse-Linked-List

---
+++ leetcode/0206-Reverse-Linked-List/source.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"fmt"
+)
+
+const (
+	topic = "Leetcode Problem 0206. 反转链表\n"
+)
+
+func init() {
+	fmt.Println(topic)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+
+	return nil
+}
