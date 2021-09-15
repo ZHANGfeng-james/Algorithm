87e6892b2b218afc5487acef9fc995b123d686f6
ZhangFeng <zfengwust3054@163.com>
2021-09-15 09:52:23 +0800

    解答：0206. 反转链表，使用迭代

--- leetcode/0206-Reverse-Linked-List/source.go
+++ leetcode/0206-Reverse-Linked-List/source.go
@@ -18,6 +18,35 @@ type ListNode struct {
 }
 
 func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
 
-	return nil
+	prev := head
+	cur := prev.Next
+	// cur != nil
+	next := cur.Next
+
+	prev.Next = nil
+	// cur.Next = prev
+	// [1,2,3,4,5] --> [5,4,3,2,1]
+	for cur != nil {
+		cur.Next = prev
+		prev = cur
+		cur = next
+		if cur != nil {
+			next = cur.Next
+		}
+	}
+
+	return prev
+}
+
+func reverse(prev *ListNode, cur *ListNode, next *ListNode) {
+	// 反转链表，只能从顺次遍历节点时开始
+	cur.Next = prev
+	// 节点平移
+	prev = cur
+	cur = next
+	next = cur.Next
 }
