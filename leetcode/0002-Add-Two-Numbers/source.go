8b1d3c3b57506d30f2ed067e0e1d2b82c203731c
ZHANGfeng-james <zfengwust3054@163.com>
2021-09-27 08:07:22 +0800

    新增：0002. 两数相加

---
+++ leetcode/0002-Add-Two-Numbers/source.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"fmt"
+)
+
+const (
+	topic = "Leetcode Problem 0002. 两数相加\n"
+)
+
+func init() {
+	fmt.Println(topic)
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	return nil
+}
