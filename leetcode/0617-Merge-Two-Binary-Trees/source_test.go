9fb57769bdfc5e3614144f5d4b9d17f81ccdff55
ZHANGfeng-james <zfengwust3054@163.com>
2021-09-27 08:54:35 +0800

    解答：0002. 两数相加；新增：0003. 无重复字符的最长子串

--- leetcode/0617-Merge-Two-Binary-Trees/source_test.go
+++ leetcode/0617-Merge-Two-Binary-Trees/source_test.go
@@ -1,7 +1,6 @@
 package leetcode
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -12,18 +11,4 @@ type question struct {
 }
 
 func TestProblem(t *testing.T) {
-
-	questions := []question{
-		{
-			1, 4, 2,
-		},
-		// add unit test case
-		{
-			1, 1, 0,
-		},
-	}
-
-	for _, q := range questions {
-		fmt.Printf("question:x=%v, y=%v, got:%v, want:%v\n", q.x, q.y, hammingDistance(q.x, q.y), q.ans)
-	}
 }
